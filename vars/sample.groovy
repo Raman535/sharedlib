3882c7158057adb98f7fc8da874cbc5ef288f2c7
Raman535 <raman535@hotmail.com>
2022-01-30 13:56:15 +0530

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-    result = !(name =~ /feature\/[0-9]+\.[0-9]+\.[0-9]+/)
+    result = false
+    if (name =~ /feature\/[0-9]+\.[0-9]+\.[0-9]+/) {
+        result=true
+    }
     echo "Hello, ${name}."
     echo "result, ${result}"
     return result
